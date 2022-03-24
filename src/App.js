8270d8890216373b4d04b534163ed1fde2d42570
YohannTisserand <tisserand.yohann@gmail.com>
2022-03-24 09:04:35 +0000

    a user can logout

--- src/App.js
+++ src/App.js
@@ -3,16 +3,17 @@ import { useState, useEffect } from 'react'
 import Note from './components/notes'
 import noteService from './services/notes'
 import loginService from './services/login'
+import LoginForm from './components/loginForm'
 
 const App = () => {
   const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(true)
   const [errorMessage, setErrorMessage] = useState(null)
-  const [succsesMessage, setSuccessMessage] = useState(null)
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
   const [user, setUser] = useState(null)
+  const [loginVisible, setLoginVisible] = useState(false)
 
   useEffect(() => {
     noteService
@@ -48,17 +49,27 @@ const App = () => {
       })
   }
 
-  const loginForm = () => (
-    <form onSubmit={handleLogin}>
+  const loginForm = () => {
+    const hideWhenVisible = { display: loginVisible ? 'none' : '' }
+    const showWhenVisible = { display : loginVisible? '' : 'none' }
+    return (
       <div>
-        username <input type='text' value={username} name='Username' onChange={({ target }) => setUsername(target.value)} />
+        <div style={hideWhenVisible}>
+          <button onClick={() => setLoginVisible(true)}>log in</button>
+        </div>
+        <div style={showWhenVisible}>
+          <LoginForm 
+          username={username}
+          password={password}
+          handleUsernameChange={({target}) => setUsername(target.value)}
+          handlePasswordChange={({target}) => setPassword(target.value)}
+          handleSubmit={handleLogin}
+          />
+          <button onClick={() => setLoginVisible(false)}>cancel</button>
+        </div>
       </div>
-      <div>
-        password <input type='text' value={password} name='Password' onChange={({ target }) => setPassword(target.value)}/>
-      </div>
-      <button type='submit'>login</button>
-    </form>
-  )
+    )
+  }
 
   const noteForm = () => (
     <form onSubmit={addNote}>
