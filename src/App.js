d13f49ad51148587757bed7e3a361d180d2a12f2
YohannTisserand <tisserand.yohann@gmail.com>
2022-03-24 10:37:20 +0000

    refactor

--- src/App.js
+++ src/App.js
@@ -6,11 +6,9 @@ import loginService from './services/login'
 import LoginForm from './components/loginForm'
 import Togglable from './components/toggable'
 import NoteForm from './components/noteFrom'
-import Logoutform from './components/logout'
 
 const App = () => {
   const [notes, setNotes] = useState([])
-  const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(true)
   const [errorMessage, setErrorMessage] = useState(null)
   const [username, setUsername] = useState('')
@@ -35,25 +33,12 @@ const App = () => {
     }
   }, [])
 
-  const addNote = (event) => {
-    event.preventDefault()
-    const noteObject = {
-      content: newNote,
-      date: new Date().toISOString(),
-      important: Math.random() > 0.5,
-      id: notes.length + 1,
-    }
-
+  const addNote = (noteObject) => {
     noteService
       .create(noteObject)
       .then(returnedNote => {
         setNotes(notes.concat(returnedNote))
-        setNewNote('')
       })
-  }
-
-  const handleNoteChange = (event) => {
-    setNewNote(event.target.value)
   }
 
   const handleLogin = async (event) => {
@@ -128,11 +113,7 @@ const App = () => {
       <p>{user.username} logged in <button onClick={handleLogout}>logout</button></p>
       
       <Togglable buttonLabel="new note">
-        <NoteForm
-          onSubmit={addNote}
-          value={newNote}
-          handleChange={handleNoteChange}
-        />
+        <NoteForm createNote={addNote} />
       </Togglable>
       </div>
     }
